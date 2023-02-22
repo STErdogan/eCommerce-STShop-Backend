287789eacdbb5f6c63c1e58c6807952232a8f5e0
Salih Tarık ERDOĞAN <salihtarike@gmail.com>
2023-02-23 02:09:52 +0300

    stshopv1.0.3 (add: Router, Rating & ProductScreen)

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,20 +8,29 @@ import {
   FormControl,
   Button, */
 } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 function Header() {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">STShop</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>STShop</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="/cart">
-                <i className="fas fa-shopping-cart"></i> Ürünler
-              </Nav.Link>
-              <Nav.Link href="/login"><i className="fas fa-user"></i> Giriş</Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart"></i> Ürünler
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <i className="fas fa-user"></i> Giriş
+                </Nav.Link>
+              </LinkContainer>
               {/*  <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
